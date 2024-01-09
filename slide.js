60ddf9c995d3cb57a4d9ac5848f01b666a4a0bc9
Bruno Araújo Pinheiro <142179481+Brunoaraujo1995@users.noreply.github.com>
2024-01-09 13:21:40 -0300

    novas atualizaçãoes

---
+++ slide.js
@@ -0,0 +1,83 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+let texto1 = document.getElementsByClassName("texto1");
+let texto2 = document.getElementsByClassName("texto2");
+
+setInterval(function () {
+    nextImage();
+}, 7000);
+
+function nextImage() {
+    count++;
+    if (count > 3) {
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+    atualizarTextos(count);
+}
+
+function atualizarTextos(indice) {
+    var textosH1 = ["Filmes Gospel", "ENCONTRO", "Gospel"];
+    var textosH2 = ["Lista mais procurada.", "Com Deus", "Premier leassons to the"];
+
+    for (var i = 0; i < texto1.length; i++) {
+        texto1[i].textContent = textosH1[indice - 1];
+    }
+
+    for (var i = 0; i < texto2.length; i++) {
+        texto2[i].textContent = textosH2[indice - 1];
+    }
+}
+
+for (var i = 1; i <= 3; i++) {
+    document.getElementById("radio" + i).addEventListener('change', function () {
+        var slideNumber = parseInt(this.id.replace('radio', ''));
+        count = slideNumber;
+
+        atualizarTextos(count);
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const videoListContainer = document.getElementById('videoListContainer');
+    let currentIndex = 0;
+    const totalVideos = 20;  // Total de vídeos
+    const videosPerPage = 1;
+
+    function showVideos() {
+        const maxIndex = Math.max(totalVideos - videosPerPage, 0);
+        currentIndex = Math.min(currentIndex, maxIndex);
+
+        const startPosition = currentIndex * -((300 + 20) * videosPerPage);
+        videoListContainer.style.transform = `translateX(${startPosition}px)`;
+    }
+
+    function nextVideo() {
+        if (currentIndex < totalVideos - videosPerPage) {
+            currentIndex++;
+            showVideos();
+        }
+    }
+
+    function prevVideo() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            showVideos();
+        }
+    }
+
+    showVideos();
+
+    window.nextVideo = nextVideo;
+    window.prevVideo = prevVideo;
+});
+
+
+
+
+
+
+
+
